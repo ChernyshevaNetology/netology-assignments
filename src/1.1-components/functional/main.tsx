8a2ac5075a3563b2a14614e018a7d572bdfed04c
Vito <example@gmail.com>
2025-04-02 14:23:44 +0600

    All fixes applied

--- src/1.1-components/functional/main.tsx
+++ src/1.1-components/functional/main.tsx
@@ -1,29 +1,34 @@
-import { ShopItemFunc } from './shop-item-func';
-import './styles.css'
-export type TItem = {brand:string, title:string, description:string, descriptionFull:string, price:number, currency:string};
+import { ShopItemFunc } from "./shop-item-func";
+import "./styles.css";
+
+export type TItem = {
+  brand: string;
+  title: string;
+  description: string;
+  descriptionFull: string;
+  price: number;
+  currency: string;
+};
 
 export const Main = () => {
-
-    const item:TItem = {
-        brand: 'Tiger of Sweden',
-        title: 'Leonard coat',
-        description: 'Minimalistic coat in cotton-blend',
-        descriptionFull: 'Men\'s minimalistic overcoat in cotton-blend. Features a stand-up collar, concealed front closure and single back vent. Slim fit with clean, straight shape. Above-knee length.',
-        price: 399,
-        currency: '£'
-    }
-    return (
-        <div className="container">
-            <div className="background-element">
-            </div>
-            <div className="highlight-window">
-                <div className='highlight-overlay'>
-                </div>
-            </div>
-            <div className="window">
-                <ShopItemFunc  item={item}/>
-            </div>
-        </div>
-    )
-
+  const item: TItem = {
+    brand: "Tiger of Sweden",
+    title: "Leonard coat",
+    description: "Minimalistic coat in cotton-blend",
+    descriptionFull:
+      "Men's minimalistic overcoat in cotton-blend. Features a stand-up collar, concealed front closure and single back vent. Slim fit with clean, straight shape. Above-knee length.",
+    price: 399,
+    currency: "£",
+  };
+  return (
+    <div className="container">
+      <div className="background-element"></div>
+      <div className="highlight-window">
+        <div className="highlight-overlay"></div>
+      </div>
+      <div className="window">
+        <ShopItemFunc product={item} />
+      </div>
+    </div>
+  );
 };
--- src/1.1-components/functional/shop-item-func.tsx
+++ src/1.1-components/functional/shop-item-func.tsx
@@ -1,27 +1,27 @@
-import { TItem} from './main';
+import { TItem } from "./main";
 
-type TProductProps = {item: TItem} // описать типы пропсов
+type TProductProps = { product: TItem }; // описать типы пропсов
 
-export const ShopItemFunc = (props:TProductProps) => {
-
-    const {brand, title, description, descriptionFull, currency, price} = props.item;
-    return (
-        <div className="main-content">
-            <h2>{brand}</h2>
-            <h1>{title}</h1>
-            <h3>{description}</h3>
-            <div className="description">
-                {descriptionFull}
-            </div>
-            <div className="highlight-window mobile">
-                <div className="highlight-overlay"></div>
-            </div>
-            <div className="divider"></div>
-            <div className="purchase-info">
-                <div className="price">{currency}{price}</div>
-
-                <button>Добавить в корзину</button>
-            </div>
+export const ShopItemFunc = ({
+  product: { brand, title, description, descriptionFull, currency, price },
+}: TProductProps) => {
+  return (
+    <div className="main-content">
+      <h2>{brand}</h2>
+      <h1>{title}</h1>
+      <h3>{description}</h3>
+      <div className="description">{descriptionFull}</div>
+      <div className="highlight-window mobile">
+        <div className="highlight-overlay"></div>
+      </div>
+      <div className="divider"></div>
+      <div className="purchase-info">
+        <div className="price">
+          {currency}
+          {price}
         </div>
-    );
+        <button>Добавить в корзину</button>
+      </div>
+    </div>
+  );
 };
